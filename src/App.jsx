80140f022d7b9e82164b78c918dd9a83867a40f6
Saaim-K <saaimkhan221@gmail.com>
2022-11-16 20:33:40 +0500

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
-import Login from './Components/Login/Login'
+// import Login from './Components/Login/Login'
 
 function App() {
   return (
     <>
-      <Login />
+      {/* <Login /> */}
+
     </>
   );
 }
